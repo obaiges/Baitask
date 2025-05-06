63fc2f8c56906d3f07ba1410ffbf7a299ea06708
obaiges <oscarbaigesr@gmail.com>
2025-05-06 20:03:14 +0200

    conexion backend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'frontend';
+
+  mensaje = '';
+
+  constructor(
+    private http: HttpClient
+  ){}
+
+  ngOnInit(): void {
+    this.http.get<{mensaje: string}>('http://localhost:3000/api/saludo').subscribe(res => {
+      this.mensaje = res.mensaje;
+    });
+  }
+  
 }
