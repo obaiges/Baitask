59165d2b72b14aa156c4b30d38e0611f7ce0ae16
obaiges <oscarbaigesr@gmail.com>
2025-05-11 22:12:56 +0200

    routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AuthComponent } from './auth/auth.component';
+import { AuthGuardService } from './auth/auth-guard.service';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: AuthComponent },
+    { path: '', component: HomeComponent, canActivate: [AuthGuardService] },
+    { path: '**', redirectTo: '' }
+];
