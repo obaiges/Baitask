83f41ffe99df53269730d919fc8185eb72c66b0f
obaiges <oscarbaigesr@gmail.com>
2025-05-15 19:49:48 +0200

    font size

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
-  darkMode:'class',
+  darkMode: 'class',
   theme: {
     extend: {},
     screens: {
@@ -13,6 +13,17 @@ module.exports = {
       xl: '1280px',
       '2xl': '1536px',
     },
+    fontSize: {
+      sm: '12px',
+      md: '14px',
+      lg: '16px',
+      xl: '18px',
+      '2xl': '20px',
+      '3xl': '24px',
+      '4xl': '28px',
+      '5xl': '32px',
+      '6xl': '36px',
+    }
   },
   plugins: [],
 }
