f4c8f419d18adb248d2a72cb5634e56b7316a888
obaiges <oscarbaigesr@gmail.com>
2025-05-15 19:49:54 +0200

    login

--- frontend/src/app/auth/auth.component.ts
+++ frontend/src/app/auth/auth.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
-  imports: [],
+  imports: [ FormsModule ],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.scss'
+  styleUrl: './auth.component.scss',
 })
 export class AuthComponent {
+
+  password?: string;
+
+  hidePassword: boolean = true;
+
+  togglePassword(): void {
+    this.hidePassword = !this.hidePassword;
+  }
+
 
 }
