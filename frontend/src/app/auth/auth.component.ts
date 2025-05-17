4c09bd25b6a13c97c0ce0e104085514a7e86ff6e
obaiges <oscarbaigesr@gmail.com>
2025-05-17 18:02:52 +0200

    login animations

--- frontend/src/app/auth/auth.component.ts
+++ frontend/src/app/auth/auth.component.ts
@@ -3,7 +3,7 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
-  imports: [ FormsModule ],
+  imports: [FormsModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss',
 })
@@ -11,10 +11,34 @@ export class AuthComponent {
 
   password?: string;
 
+  login = true;
+
   hidePassword: boolean = true;
 
   togglePassword(): void {
     this.hidePassword = !this.hidePassword;
+  }
+
+  swapLogin() {
+    const left = document.getElementById("left")!;
+    const right = document.getElementById("right")!;
+    const textHeader = document.getElementById("textHeader")!;
+
+    if (this.login) {
+      // Mover a la derecha
+      left.style.transform = 'translateX(100%)';
+      right.style.transform = 'translateX(-100%)';
+    } else {
+      left.style.transform = 'translateX(0)';
+      right.style.transform = 'translateX(0)';
+    }
+    textHeader.style.opacity = '0';
+    setTimeout(() => {
+      textHeader.textContent = this.login ? "Iniciar sesión" : "Registrarse";
+      textHeader.style.opacity = '1';
+    }, 300); // Espera a que termine el fade out
+
+    this.login = !this.login;
   }
 
 
